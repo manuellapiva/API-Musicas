d314260cf2c2dd2e3d0da514796ab632c7670f26
ayla-vilela <ayla.vilela51@gmail.com>
2026-03-02 15:47:13 -0300

    POST

--- api_musicas.js
+++ api_musicas.js
@@ -47,6 +47,25 @@ app.get("/musicas/:id", (req, res) => {
     }
 });
 
+app.post("/musicas", (req, res) => {
+    const {nomemusica, autor, link} = req.body;
+
+    if (!nomemusica || !autor || !link) {
+        return res.status(400).json({ msg: "Dados Incompletos" });
+    }
+
+    const novo = {
+        id: proximoId++,
+        nomemusica,
+        autor,
+        link
+    };
+
+    musicas.push(novo);
+    res.status(201).json(novo);
+});
+
+
 app.put("/musicas/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const {nomemusica, autor, link} = req.body;
