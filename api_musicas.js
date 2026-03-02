3437f19f9b0090830a11dce9782bdeb139ea167f
o guti <gustavo.millamonte10@gmail.com>
2026-03-02 15:57:00 -0300

    changing to lower case

--- api_musicas.js
+++ api_musicas.js
@@ -48,8 +48,8 @@ app.get("/musicas/:id", (req, res) => {
 });
 
 app.get("/musicas/nome/:nomeid", (req, res) => {
-    const nome = req.params.nomeid;
-    const nomeID = musicas.find((n) => n.nome === nome);
+    const nome = req.params.nomeid.toLowerCase();
+    const nomeID = musicas.find((n) => n.nome.toLowerCase() === nome);
     if (nomeID) {
         res.status(200).json(nomeID);
     } else {
